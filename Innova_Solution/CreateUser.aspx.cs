1223a02bb6cbdea0f9e27145a33a1bb3984808a6
Carlos Oreamuno <carloso0996@gmail.com>
2018-12-05 00:00:15 -0600

    Regex for create user

--- Innova_Solution/CreateUser.aspx.cs
+++ Innova_Solution/CreateUser.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 // Data
 using System.Data;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 public partial class CreateUser : System.Web.UI.Page
 {
@@ -18,8 +19,12 @@ public partial class CreateUser : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
+        string password = (@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"); // Regex para la contraseña
+        Match pass = Regex.Match(TxtPassWord.Text, password); //La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.
+
         string nameUser = TxtUserName.Text;
         string passwordUser = TxtPassWord.Text;
+        string pass2 = txtPass2.Text;
         string mail = TxtMail.Text;
         int telefono=int.Parse(TextTel.Text);
         string profesion= TextProf.Text;
@@ -27,10 +32,16 @@ public partial class CreateUser : System.Web.UI.Page
         int id = userServices.lastId();
         bool UsersAux = userServices.CreateUser(nameUser, passwordUser, id, mail, telefono, profesion, vivienda);
 
-        if (UsersAux == true)
+        if (pass.Success)
+        {
+            Response.Write("<script>alert('Escriba al menos un caracter especial y una mayúscula en la contraseña');</script>");
+        }else if (passwordUser != pass2)
+        {
+            Response.Write("<script>alert('Las contraseñas deben de coincidir');</script>");
+        }
+        else if (UsersAux == true)
         {
             Response.Write("<script>alert('Usuario Creado');</script>");
-
         }
         else
         {
@@ -42,5 +53,7 @@ public partial class CreateUser : System.Web.UI.Page
     {
         Response.Redirect("Index.aspx");
     }
+
+
 
 }
