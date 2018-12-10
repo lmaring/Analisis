f2ddee2b7fb4580b3a5ccdfd08aeab362d56d964
Luis Arturo Marin Godinez <40041521+lmaring@users.noreply.github.com>
2018-12-10 13:35:03 -0600

    notas

--- Innova_Solution/Grades.aspx.cs
+++ Innova_Solution/Grades.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class Grades : System.Web.UI.Page
 {
     TestServices aux = new TestServices();
-    Tests test = null;
+    List<Grade> notas = new List<Grade>();
+    Grade grade = null;
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        User usuario = (User)Session["User"];
+        notas = aux.selectNotas(usuario.Id);
+            grade =notas[i];
+            GridView1.DataSource = notas;
     }
 }
